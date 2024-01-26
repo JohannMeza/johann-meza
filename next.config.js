cddebfc8ff2d2106b9c59ea1e938384ba5427f00
JohannMeza <johanmezasalazar@gmail.com>
2024-01-26 14:47:29 -0500

    Fix | Wbebpack

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { domains: ['res.cloudinary.com', 'cdn.pixabay.com'], formats: ['image/avif', 'image/webp'], },
-  future: {
-    webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
-      // Looks like backward compatibility approach.
-  },
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
