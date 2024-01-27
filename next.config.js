96953076325df3cad76aa8dc9affa4a30085d56c
JohannMeza <johanmezasalazar@gmail.com>
2024-01-26 23:15:06 -0500

    Deploy | Second

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: { domains: ['res.cloudinary.com', 'cdn.pixabay.com', ''], formats: ['image/avif', 'image/webp'], },
+  images: { domains: ['res.cloudinary.com', 'cdn.pixabay.com'], formats: ['image/avif', 'image/webp'], },
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
@@ -12,20 +12,6 @@ const nextConfig = {
 
     return config;
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
